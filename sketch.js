68ee2ec0c4974f89862b80c255638c1ae417bd9d
20profmatheus <108628651+20profmatheus@users.noreply.github.com>
2025-04-22 19:52:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("grey");
+  textSize (40);
+  text ("😇",0,100);
+  text ("🧑‍🎄",0,300);
+  rect (350,0,10,400);
+}
